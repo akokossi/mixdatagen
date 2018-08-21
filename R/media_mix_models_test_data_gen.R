102ac3ca6b4612436a3bc4eba47dd7bd08d446a8
Alphonse Okossi <okossial@gmail.com>
2018-08-20 22:31:51 -0400

    test2

--- R/media_mix_models_test_data_gen.R
+++ R/media_mix_models_test_data_gen.R
@@ -5,41 +5,89 @@
 #
 
 
-gen_agg_dates_dimension<-function(nDays,startDate)
+gen_digital_addressable_media_dimension<-function(nChannels=0,modeledEventType="Impressions")
+{
+
+}
+gen_offline_non_addressable_media_dimension<-function(nChannels=0,modeledEventType="Impressions")
 {
 
 }
 
-gen_digital_addressable_media_dimension<-function(nChannels,nEventTypes)
+gen_device_dimension<-function(nDevices=0)
 {
+  if(nDevices==0){
+  dat<-data.frame(device_id=1,device_name="One-device")
+  }else{
 
-}
-gen_offline_non_addressable_media_dimension<-function(nChannels,nEventTypes)
-{
+    dat<-data.frame(device_id=1:nDevice,device_name=paste0("Device", seq(1:nDevices), sep=""))
 
-}
-
-gen_device_dimension<-function(nDevices)
-{
-
+  }
+ return(dat)
 }
 
 
 
-gen_markets_dimension<-function(nMarkets,country)
+gen_markets_dimension<-function(nMarkets=0,country)
 {
+  if(nMarkets==0){
+    dat<-data.frame(country=country,market_id=1,market_name="One-market")
+  }else{
+
+    dat<-data.frame(country=rep(country,nMarkets),market_id=1:nMarkets,market_name=paste0("Market", seq(1:nMarkets), sep=""))
+
+  }
+  return(dat)
+}
+
+gen_conversion_events_dimension<-function(nConversionEvents=0)
+{
+  if(nConversionEvents==0){
+    dat<-data.frame(conversion_event_id=1,conversion_event_name="One-Conversion Event")
+  }else{
+
+    dat<-data.frame(conversion_event_id=1:nConversionEvents,conversion_event_name=paste0("Conversion event ", seq(1:nConversionEvents), sep=""))
+
+  }
+  return(dat)
+}
+
+
+
+gen_agg_dates_dimension<-function(nDays,nMarkets,startDate)
+{
+  # determine demand seasonality
+  # output:
+  #
+  #   marketid:
+  #   date
+  #   demand_seasonindex <- probability of demand by day
+  #
+}
+
+gen_agg_execution_schedule<-function(nDays,nMarkets,nChannels,startDate)
+{ # determine paid marketing executions by day
+  # output:
+  #
+  #   marketid:
+  #   date
+  #   paid mkg channel id
+  #   executions_weight <- probability of executing in market and day
+  #
+}
+
+gen_conversion_method<-function()
+{
+
 
 }
 
-gen_conversion_events_dimension<-function(nConversionEvents)
-{
-
-}
 
 gen_journey_by_market_dist<-function(nConversionEvents,nMarkets,localNMarkets=2)
 {
 
 }
+
 
 
 gen_one_journey<-function(marketIndexSet,
